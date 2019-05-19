6c0735ca84f7f384abd1383b50b30181a2992e28
hung <dthung1602@gmail.com>
2019-05-19 11:28:25 +0700

    Add react build to deploy

---
+++ react-mb-client/build/precache-manifest.c3e60d3ed3ecb8ea22f25546d2c8f125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4978072286a496681a17cb68d5f8d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d2e5e68cebb888af949",
+    "url": "/static/css/main.e28fd9da.chunk.css"
+  },
+  {
+    "revision": "1f5d4c6f16b52b29fb45",
+    "url": "/static/js/2.e0dd2212.chunk.js"
+  },
+  {
+    "revision": "9d2e5e68cebb888af949",
+    "url": "/static/js/main.a4f4eca5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7a65b4278e2b4c61d2077f464530ec1",
+    "url": "/static/media/logo.b7a65b42.png"
+  }
+]);
