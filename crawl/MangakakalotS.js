28fbeac241c110b7eba9df5e56364ba0e737a6d4
Duong Thanh Hung <hung.duong@nfq.asia>
2020-04-07 23:13:22 +0700

    Add mangakakalotS

---
+++ crawl/MangakakalotS.js
@@ -0,0 +1,37 @@
+const normalizeDataSource = require("./utils").normalizeDataSource;
+
+const URLRegex = /^https?:\/\/mangakakalot\.com\/read-.+$/;
+
+async function parseChapters(dataSource) {
+  const $ = await normalizeDataSource(dataSource);
+
+  const rows = $(".chapter-list a");
+
+  const chapters = [];
+  for (let i = 0; i < rows.length; i++) {
+    chapters.push({
+      name: rows[i].children[0].data,
+      link: rows[i].attribs.href,
+    });
+  }
+
+  return chapters;
+}
+
+async function parseManga(dataSource) {
+  const $ = await normalizeDataSource(dataSource);
+
+  return {
+    name: $("h1").text(),
+    link: $('meta[property="og:url"]').attr("content"),
+    image: $(".manga-info-pic img").attr("src"),
+    isCompleted: $(".manga-info-text li")[2].children[0].data === "Status : Completed",
+    chapters: await parseChapters($),
+  };
+}
+
+module.exports = {
+  URLRegex,
+  parseManga,
+  parseChapters,
+};
