66f073af91f1a4b2e7543e9d5031a588107ffc5e
dthung1602 <dthung1602@gmail.com>
2021-01-04 20:20:07 +0700

    Update background

--- frontend/src/assets/background/index.js
+++ frontend/src/assets/background/index.js
@@ -1,28 +1,40 @@
+import image0 from "./somali-and-the-forest-spirit.jpg";
+import image1 from "./witch-hat-atelier.jpeg";
+import image2 from "./attack-on-titan-4.png";
 import image3 from "./attack-on-titan-3.jpg";
+import image4 from "./attack-on-titan.jpg";
+import image5 from "./stein-gate.jpg";
 import image6 from "./bleach-2.jpg";
 import image7 from "./demon-slayer.png";
 import image8 from "./fulmetal-alchemist-1.jpg";
+import image9 from "./mahou-tsukai-no-yome.jpg";
 import image10 from "./hinamatsuri.jpg";
 import image11 from "./inuyasha-1.jpg";
+import image12 from "./bakemonogatari.jpg";
 import image13 from "./kaguyasama-love-is-war.png";
 import image14 from "./naruto.jpg";
 import image15 from "./noragami.jpg";
-import image16 from "./sword-art-online.jpg";
 import image17 from "./umaru.jpg";
 import image18 from "./violet-evergarden-1.jpg";
 import image19 from "./violet-evergarden-2.jpg";
 
 export default [
+  image0,
+  image1,
+  image2,
   image3,
+  image4,
+  image5,
   image6,
   image7,
   image8,
+  image9,
   image10,
   image11,
+  image12,
   image13,
   image14,
   image15,
-  image16,
   image17,
   image18,
   image19,
