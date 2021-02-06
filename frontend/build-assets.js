226cc49dfdf91025ba8d80f71eed63441a393908
dthung1602 <dthung1602@gmail.com>
2021-02-06 17:06:56 +0700

    Convert sprite from png to webp

--- frontend/build-assets.js
+++ frontend/build-assets.js
@@ -6,13 +6,16 @@
  */
 const fs = require("fs");
 const Spritesmith = require("spritesmith");
+const { execSync } = require("child_process");
 
-const SPRITE_FILE = "sitelogo-sprite.png";
-const CSS_FILE = "sitelogo-sprite.css";
+const PNG_SPRITE_FILE_NAME = "sitelogo-sprite.png";
+const CSS_FILE_NAME = "sitelogo-sprite.css";
+const PNG_SPRITE_PATH = `${__dirname}/src/assets/sitelogo/${PNG_SPRITE_FILE_NAME}`;
+const CSS_PATH = `${__dirname}/src/assets/sitelogo/${CSS_FILE_NAME}`;
 
 const imagePaths = fs
   .readdirSync(`${__dirname}/src/assets/sitelogo`)
-  .filter((f) => !f.includes(SPRITE_FILE) && !f.includes(CSS_FILE))
+  .filter((f) => !f.includes(PNG_SPRITE_FILE_NAME) && !f.includes(CSS_FILE_NAME))
   .map((f) => `${__dirname}/src/assets/sitelogo/${f}`);
 
 const getSiteFromImageFilePath = (fullPath) => {
@@ -32,15 +35,29 @@ const generateCSS = (coordinates) => {
   return css.replace(/ 0px/g, " 0");
 };
 
+const secret = process.env["CONVERT_API_SECRET"];
+const WEBP_SPRINT_PATH = PNG_SPRITE_PATH.replace(".png", ".webp");
+const convertToWebpCmd = `curl -s -F "File=@${PNG_SPRITE_PATH}" https://v2.convertapi.com/convert/png/to/webp?Secret=${secret} > ${WEBP_SPRINT_PATH}`;
+console.log(convertToWebpCmd);
+
+const convertToWebp = () => {
+  execSync(convertToWebpCmd);
+  const fileData = JSON.parse(fs.readFileSync(WEBP_SPRINT_PATH)).Files[0].FileData;
+  const buff = new Buffer(fileData, "base64");
+  fs.writeFileSync(WEBP_SPRINT_PATH, buff);
+};
+
 console.log("Generating sprites...");
 Spritesmith.run({ src: imagePaths, padding: 1 }, function (err, result) {
   if (err) {
     console.error(err);
   } else {
     console.log("Writing sprite to file...");
-    fs.writeFileSync(`${__dirname}/src/assets/sitelogo/${SPRITE_FILE}`, result.image);
+    fs.writeFileSync(PNG_SPRITE_PATH, result.image);
+    console.log("Converting to webp...");
+    convertToWebp();
     console.log("Writing sprite css...");
-    fs.writeFileSync(`${__dirname}/src/assets/sitelogo/${CSS_FILE}`, generateCSS(result.coordinates));
+    fs.writeFileSync(CSS_PATH, generateCSS(result.coordinates));
     console.log("Done generating sprites...");
   }
 });
