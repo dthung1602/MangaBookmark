8ee211190ed0c7bd0192275843ce509aea6d4309
dthung1602 <dthung1602@gmail.com>
2021-02-11 15:17:50 +0700

    Upgrade dependencies & remove FE packages in production build

--- build.sh
+++ build.sh
@@ -25,11 +25,21 @@ echo "Done compressing"
 if [ "${NODE_ENV}" = "production" ]
 then
   # move build to backend
+  echo "Moving frontend build directory to backend"
   mv "${DIR}/frontend/build" "${DIR}/backend/frontend-build"
+
   # remove the frontend dir to make the build lighter
+  echo "Removing frontend directory"
   rm -rf "${DIR}/frontend/"
+
+  # remove frontend packages in node_modules to make the build even lighter
+  echo "Removing frontend packages"
+  frontend_packages=$(node -e "console.log(Object.keys(require('./frontend/package.json').dependencies))" | tr -d "[],'\"\n")
+  yarn --cwd "${DIR}/frontend" remove $frontend_packages
+
 else
   # create soft link if it does not exist yet
+  echo "Creating symlink to frontend build directory"
   if [ ! -L "${DIR}/backend/frontend-build" ]
   then
     ln -s "${DIR}/frontend/build" "${DIR}/backend/frontend-build"
