dca47370244d80573ddfc60a6c26899454fd80d8
dthung1602 <dthung1602@gmail.com>
2021-06-24 22:31:15 +0700

    Additional field for MangaHasu

--- backend/services/manga-service/parsers/MangaHasu.js
+++ backend/services/manga-service/parsers/MangaHasu.js
@@ -1,6 +1,34 @@
-const { fetchAndLoad, useImageProxy } = require("./utils");
+const {
+  fetchAndLoad,
+  extractNamesFromText,
+  extractTagsFromText,
+  extractAuthorsFromText,
+  useImageProxy,
+} = require("./utils");
 
 const URLRegex = /^https?:\/\/mangahasu\.se\/.+/;
+
+function getInfoRowByTitle($, title) {
+  let value = "";
+  $(".box-des .detail_item").each((i, e) => {
+    const text = $(e).text().trim();
+    if (text.includes(title)) {
+      value = text;
+    }
+  });
+  return value;
+}
+
+function parseAdditionalInfo($) {
+  const description = $(".wrapper_content > .content-info").text().trim();
+  const alternativeNames = extractNamesFromText($(".info-title h3").text(), ";");
+  const tags = extractTagsFromText(getInfoRowByTitle($, "Genre"), ",", "Genre(s):");
+  const authors = [
+    ...extractAuthorsFromText(getInfoRowByTitle($, "Author"), ";", "Author(s)"),
+    ...extractAuthorsFromText(getInfoRowByTitle($, "Artist"), ";", "Artist(s)"),
+  ];
+  return { description, alternativeNames, authors, tags };
+}
 
 async function parseChapters($) {
   const rows = $(".list-chapter a");
@@ -26,8 +54,124 @@ async function parseManga(url) {
     image: useImageProxy(image, "MangaHasu"),
     isCompleted: $(".detail_item a").text().includes("Completed"),
     chapters: await parseChapters($),
+    ...parseAdditionalInfo($),
   };
 }
+
+const availableTags = [
+  "4-koma",
+  "action",
+  "adaptation",
+  "adult",
+  "adventure",
+  "aliens",
+  "animals",
+  "anime",
+  "anthology",
+  "award winning",
+  "bara",
+  "comedy",
+  "comic",
+  "cooking",
+  "crime",
+  "crossdressing",
+  "delinquents",
+  "demons",
+  "doujinshi",
+  "drama",
+  "ecchi",
+  "fan colored",
+  "fantasy",
+  "full color",
+  "game",
+  "gender bender",
+  "genderswap",
+  "ghosts",
+  "gore",
+  "gyaru",
+  "harem",
+  "historical",
+  "horror",
+  "incest",
+  "isekai",
+  "josei",
+  "live action",
+  "loli",
+  "lolicon",
+  "long strip",
+  "mafia",
+  "magic",
+  "magical girls",
+  "manga reviews",
+  "martial arts",
+  "mature",
+  "mecha",
+  "medical",
+  "military",
+  "monster girls",
+  "monsters",
+  "music",
+  "mystery",
+  "netorare/ntr",
+  "ninja",
+  "office",
+  "office workers",
+  "official colored",
+  "one shot",
+  "others",
+  "philosophical",
+  "police",
+  "post-apocalyptic",
+  "psychological",
+  "reincarnation",
+  "reverse harem",
+  "romance",
+  "samurai",
+  "school",
+  "school life",
+  "school life. seinen",
+  "sci-fi",
+  "seinen",
+  "seinen  supernatural",
+  "sexual violence",
+  "shota",
+  "shotacon",
+  "shoujo",
+  "shoujo ai",
+  "shoujoai",
+  "shounen",
+  "shounen ai",
+  "shounenai",
+  "slice of life",
+  "sm/bdsm",
+  "smut",
+  "sports",
+  "super power",
+  "superhero",
+  "supernatural",
+  "survival",
+  "thriller",
+  "time travel",
+  "traditional games",
+  "tragedy",
+  "uncategorized",
+  "user created",
+  "vampire",
+  "vampires",
+  "video games",
+  "villainess",
+  "violence",
+  "virtual reality",
+  "web comic",
+  "webtoon",
+  "webtoons",
+  "western",
+  "wuxia",
+  "yaoi",
+  "youkai",
+  "yuri",
+  "zombies",
+];
 
 module.exports = {
   active: true,
@@ -37,4 +181,6 @@ module.exports = {
   URLRegex,
   parseManga,
   parseChapters,
+  parseAdditionalInfo,
+  availableTags,
 };
