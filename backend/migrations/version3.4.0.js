f56f0eb052ad87fcf9cdd7566a92851d2e8350db
dthung1602 <dthung1602@gmail.com>
2021-06-27 14:45:31 +0700

    Update migration script

--- backend/migrations/version3.4.0.js
+++ backend/migrations/version3.4.0.js
@@ -5,20 +5,26 @@
  */
 
 const mongoose = require("mongoose");
+const { chunk } = require("lodash");
 
 const { Manga } = require("../models");
-const { DB_URL } = require("../config");
+const { DB_URL, CRAWL_CONCURRENCY } = require("../config");
+const { fetch } = require("../services/manga-service/parsers/utils");
 const MangaService = require("../services/manga-service");
 
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 
+async function updateRedirectURL(obj) {
+  obj.link = (await fetch(obj.link)).url;
+}
+
 async function main() {
   console.log("Connecting to database");
   await mongoose.connect(DB_URL, { useUnifiedTopology: true });
 
-  console.log("Fetch mangas");
+  console.log("Fetch nettruyen mangas");
   const nettruyenMangas = await Manga.find({ site: "NetTruyen" });
   console.log(`Found ${nettruyenMangas.length} mangas`);
 
@@ -33,6 +39,29 @@ async function main() {
 
   console.log("Updating");
   await MangaService.updateMultiple(nettruyenMangas, true, true);
+
+  console.log("Fetch mangadex mangas");
+  const mangadexMangas = await Manga.find({ site: "MangaDex" });
+  console.log(`Found ${mangadexMangas.length} mangas`);
+
+  const chunks = chunk(mangadexMangas, CRAWL_CONCURRENCY);
+
+  for (let i = 0; i < chunks.length; i++) {
+    await Promise.all(
+      chunks[i].map(async (manga) => {
+        console.log(`    Update url of ${manga.name}`);
+        await updateRedirectURL(manga);
+        const chapterChunks = chunk(manga.chapters, CRAWL_CONCURRENCY);
+        for (let j = 0; j < chapterChunks.length; j++) {
+          console.log(`       [${j}] Update chapters of ${manga.name}`);
+          await Promise.all(chapterChunks[j].map(updateRedirectURL));
+        }
+      }),
+    );
+  }
+
+  console.log("Updating");
+  await MangaService.updateMultiple(mangadexMangas, true, true);
 
   console.log("Done");
   await mongoose.connection.close();
