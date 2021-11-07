e420876d60877e60e3457dc955d98f836fc07aef
dthung1602 <dthung1602@gmail.com>
2021-11-07 15:59:43 +0700

    Change upload avatar UI

--- frontend/src/components/Account/BasicInfo.js
+++ frontend/src/components/Account/BasicInfo.js
@@ -1,18 +1,42 @@
 import { useContext, useState } from "react";
-import { Button, Form, Input, message } from "antd";
+import { Button, Form, Input, Upload, Spin, message } from "antd";
 import { MailOutlined, UserOutlined } from "@ant-design/icons";
 
 import { UserAPI } from "../../api";
-import AvatarUploader from "./AvatarUploader";
 import { GlobalContext } from "../GlobalContext";
 import { throwOnCriticalErrors, notifyError } from "../../utils/error-handler";
+import ImgCrop from "antd-img-crop";
 
 const BasicInfo = () => {
-  const [isLoading, setIsLoading] = useState(false);
   const [{ user }, updateGlobalContext] = useContext(GlobalContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAvatarUploading, setIsAvatarUploading] = useState(false);
+  const [avatarError, setAvatarError] = useState(null);
+  const [avatarUrl, setAvatarUrl] = useState(user.avatar);
+
+  const handleUploadEvent = ({ file }) => {
+    setIsAvatarUploading(file.status === "uploading");
+
+    if (file.status === "error") {
+      setAvatarError("Some thing went wrong");
+    } else {
+      setAvatarError(null);
+    }
+
+    if (file.status === "done") {
+      if (file.response.status === "error") {
+        setAvatarError(file.response.error);
+      }
+      setAvatarUrl(file.response.url);
+      return file.response.url;
+    }
+    return avatarUrl;
+  };
 
   const onFinish = (values) => {
     setIsLoading(true);
+    values.avatar = avatarUrl; // ImgCrop does not play nice with ant Form -> a small hack here
+
     UserAPI.patch(values)
       .result.then(async (response) => {
         throwOnCriticalErrors(response);
@@ -48,11 +72,39 @@ const BasicInfo = () => {
           <Input prefix={<MailOutlined className="site-form-item-icon" />} />
         </Form.Item>
 
-        <Button type="primary" htmlType="submit" loading={isLoading} className="float-right">
+        <Form.Item
+          name="avatar"
+          label="Avatar"
+          hasFeedback={Boolean(avatarError)}
+          validateStatus={avatarError ? "error" : undefined}
+          help={avatarError}
+        >
+          <ImgCrop rotate shape="round">
+            <Upload
+              name="file"
+              maxCount={1}
+              listType="picture-card"
+              showUploadList={false}
+              action="/api/user/avatar"
+              onChange={handleUploadEvent}
+            >
+              <Spin spinning={isAvatarUploading}>
+                <img width={"80px"} src={avatarUrl} alt="avatar" />
+              </Spin>
+            </Upload>
+          </ImgCrop>
+        </Form.Item>
+
+        <Button
+          type="primary"
+          htmlType="submit"
+          disabled={isAvatarUploading}
+          loading={isLoading}
+          className="float-right"
+        >
           Update
         </Button>
       </Form>
-      <AvatarUploader />
     </>
   );
 };
