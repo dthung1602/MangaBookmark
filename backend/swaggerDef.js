2396d04b1d345453b4f55ee880cab17a072292b8
dthung1602 <dthung1602@gmail.com>
2022-03-02 22:55:50 +0700

    Update tag to v4.0.0

--- backend/swaggerDef.js
+++ backend/swaggerDef.js
@@ -2,7 +2,7 @@ module.exports = {
   openapi: "3.0.0",
   info: {
     title: "MangaBookmark API",
-    version: "4.0.0-beta-2",
+    version: "4.0.0",
     description: "API for MangaBookmark client",
   },
   apis: ["api/**/*.js", "models/**/*.js"],
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,7 @@
 // ------------------------------------------
 //       General
 // ------------------------------------------
-export const FRONTEND_VERSION = "4.0.0-beta-2";
+export const FRONTEND_VERSION = "4.0.0";
 export const ANY = "any";
 export const MANGA_PER_PAGE = 5;
 export const RIGHT_PANEL_TABLE_PAGE_SIZE = 20;
