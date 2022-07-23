e3adb1b27ab8204f24dead9fe244b50a6a79eadf
dthung1602 <dthung1602@gmail.com>
2022-07-23 13:31:58 +0700

    Add more background image

--- frontend/src/assets/background/index.js
+++ frontend/src/assets/background/index.js
@@ -1,9 +1,12 @@
+import image1 from "./attack-on-titan-3.webp";
 import image3 from "./attack-on-titan.jpg";
+import image4 from "./call-of-the-night.jpg";
 import image5 from "./stein-gate.webp";
 import image6 from "./bleach.jpg";
 import image7 from "./demon-slayer.webp";
 import image8 from "./fulmetal-alchemist.jpg";
 import image9 from "./mahou-tsukai-no-yome.webp";
+import image10 from "./call-of-the-night-2.jpg";
 import image11 from "./attack-on-titan-ss4.webp";
 import image12 from "./bakemonogatari.webp";
 import image13 from "./kaguyasama-love-is-war.webp";
@@ -13,12 +16,15 @@ import image17 from "./made-in-abyss.jpg";
 import image18 from "./violet-evergarden.webp";
 
 export default [
+  image1,
   image3,
+  image4,
   image5,
   image6,
   image7,
   image8,
   image9,
+  image10,
   image11,
   image12,
   image13,
