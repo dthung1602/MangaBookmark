cad7b3f784b55bcccae4d866182738809c6f2b35
hung <dthung1602@gmail.com>
2022-09-04 20:31:12 +0700

    Stream background service log to main app

--- backend/services/background-service.js
+++ backend/services/background-service.js
@@ -3,17 +3,25 @@ const { spawn } = require("child_process");
 const BIN_DIR = `${__dirname}/../bin`;
 
 function startUpdateMangaProcess() {
-  spawn("node", [`${BIN_DIR}/update_mangas`], {
-    detached: true,
-    env: { ...process.env },
-  });
+  // TODO Ensure only one background process is running
+  startBackgroundScript("node", [`${BIN_DIR}/update_mangas`]);
 }
 
 function startBackupProcess() {
-  spawn("bash", [`${BIN_DIR}/backup_db.bash`], {
+  // FIXME cannot download to current working dir
+  startBackgroundScript("bash", [`${BIN_DIR}/backup_db.bash`]);
+}
+
+function startBackgroundScript(entrypoint, args) {
+  const child = spawn(entrypoint, args, {
     detached: true,
     env: { ...process.env },
   });
+
+  child.stdout.setEncoding("utf-8");
+  child.stdout.on("data", console.log);
+  child.stderr.setEncoding("utf-8");
+  child.stderr.on("data", console.error);
 }
 
 module.exports = { startUpdateMangaProcess, startBackupProcess };
