7f02605362ea2006982160a3733fc4803592a224
hung <dthung1602@gmail.com>
2025-12-20 17:23:19 +0700

    Update dep

--- frontend/src/utils/service-worker-registry.js
+++ frontend/src/utils/service-worker-registry.js
@@ -17,10 +17,10 @@ const DISABLE_SW_ON_LOCALHOST = false;
 
 const isLocalhost = Boolean(
   window.location.hostname === "localhost" ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === "[::1]" ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === "[::1]" ||
+  // 127.0.0.0/8 are considered localhost for IPv4.
+  window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 
 /**
